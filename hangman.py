9345e74432d3e9f4e255430d5974af5f75a3ea3e
Yuji Inoue <y.inoue@powerplace.co.jp>
2021-11-04 11:57:27 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,39 @@
+def hangman(word):
+    wrong = 0
+    stages = ["", 
+            "__________          ",
+            "|                   ",
+            "|         |         ",
+            "|         O         ",
+            "|        /|`        ",
+            "|        / `        ",
+            "|                   "
+            ]
+    rletters = list(word)
+    print(rletters)
+    board = ["_"] * len(word)
+    print(board)
+    win = False
+    print("Welcome to HangMan")
+    while wrong < len(stages)-1:
+        print("\n")
+        msg = "Image a char"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+            print(rletters)
+        else:
+            wrong +=1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("You win !")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("you lose! Answer is {}.".format(word))
